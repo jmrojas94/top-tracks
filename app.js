7d1d202c4c35ec6e32dc6a687ff1f86dc6a0fc23
Juan Rojas <jmr5150@gmail.com>
2017-01-17 02:02:34 -0600

    Fixes callback url.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var cookieParser = require('cookie-parser');
 
 var client_id = process.env.CLIENT_ID; // Your client id
 var client_secret = process.env.CLIENT_SECRET; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var redirect_uri = 'https://spottoptracks.herokuapp.com/'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
