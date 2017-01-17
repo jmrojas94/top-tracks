87adb019165bf84ddf2da44ebdc387114a28803a
Juan Rojas <jmr5150@gmail.com>
2017-01-17 01:58:01 -0600

    Bug fixing.

--- app.js
+++ app.js
@@ -150,5 +150,6 @@ app.get('/logout', function(req, res) {
   res.redirect('/');
 });
 
-console.log('Listening on 8888');
-app.listen(8888);
+app.listen(process.env.PORT || 8888, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
